5e0ac245fb18ba66a5b43cf1f672f68b55107c45
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-21 09:37:35 -0400

    Update and rename IdAI/main/main.py to IdAI/main/train/main.py

---
+++ IdAI/main/train/main.py
@@ -0,0 +1,16 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow.keras import layers
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+import pandas as pd
+
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+print(tf.__version__)
+
