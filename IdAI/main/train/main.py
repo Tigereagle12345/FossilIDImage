2da3e5cb6773f87bd8b363e6e7a20521d6fccbe4
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-10-21 10:11:43 -0400

    Update main.py

--- IdAI/main/train/main.py
+++ IdAI/main/train/main.py
@@ -14,3 +14,28 @@ np.set_printoptions(precision=3, suppress=True)
 
 print(tf.__version__)
 
+fossils = pd.read_csv("FossilAITestDataSet.csv")
+fossils.head()
+fossil_features = fossils.copy()
+
+inputs = {}
+
+for name, column in fossil_features.items():
+  dtype = column.dtype
+  if dtype == object:
+    dtype = tf.string
+  else:
+    dtype = tf.float32
+
+  inputs[name] = tf.keras.Input(shape=(1,), name=name, dtype=dtype)
+
+print(inputs)
+numeric_inputs = {name:input for name,input in inputs.items()
+                  if input.dtype==tf.float32}
+
+x = layers.Concatenate()(list(numeric_inputs.values()))
+norm = layers.Normalization()
+norm.adapt(np.array(titanic[numeric_inputs.keys()]))
+all_numeric_inputs = norm(x)
+
+print("all_numeric_inputs")
